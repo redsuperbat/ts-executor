1801b6f0e2b59dc4658d9eb756ad4d335f486c08
Max Netterberg <max.netterberg@gmail.com>
2023-03-21 09:54:48 +0100

    inital commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM node:latest as builder
+
+WORKDIR /app
+
+COPY package*.json .
+
+RUN npm install
+
+COPY . .
+
+RUN npm run build
+
+FROM node:latest as runner
+
+WORKDIR /app
+
+COPY --from=builder /app/node_modules node_modules
+
+COPY --from=builder /app/index.js index.js
+
+CMD [ "node", "index.js" ]
---
+++ index.ts
@@ -0,0 +1,50 @@
+import { transpile } from "typescript";
+
+import { exec } from "child_process";
+import { randomUUID } from "crypto";
+import init from "fastify";
+import { rm, writeFile } from "fs/promises";
+const fastify = init({ logger: false });
+
+function cmd(command: string) {
+  let p = exec(command);
+  return new Promise<string>((res) => {
+    let stdout = "";
+    p.stdout?.on("data", (x) => {
+      stdout += x.toString();
+    });
+    p.stderr?.on("data", (x) => {
+      stdout += x.toString();
+    });
+    p.on("exit", () => {
+      res(stdout);
+    });
+  });
+}
+
+fastify.post("/run", async (req, _) => {
+  const tsCode = String(req.body);
+  console.log(tsCode);
+
+  const js = transpile(tsCode);
+  console.log(js);
+  const filename = randomUUID();
+  await writeFile(filename, js);
+  try {
+    const output = await cmd(`node ./${filename}`);
+    console.log(output);
+    return output;
+  } finally {
+    await rm(filename);
+  }
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
