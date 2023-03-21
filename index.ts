439f2df31ad9df5703c8a460f86d5760c5808ca8
Max Netterberg <max.netterberg@gmail.com>
2023-03-21 15:52:20 +0100

    Fixed stuff

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { transpile } from "typescript";
+import { ModuleKind, ScriptTarget, transpile } from "typescript";
 
 import { exec } from "child_process";
 import { randomUUID } from "crypto";
@@ -10,14 +10,15 @@ function cmd(command: string) {
   let p = exec(command);
   return new Promise<string>((res) => {
     let stdout = "";
-    p.stdout?.on("data", (x) => {
-      stdout += x.toString();
-    });
-    p.stderr?.on("data", (x) => {
-      stdout += x.toString();
-    });
+    const append = (data: any) => {
+      data = data.toString() as string;
+      if (data.includes(" npm i ")) return;
+      stdout += data;
+    };
+    p.stdout?.on("data", append);
+    p.stderr?.on("data", append);
     p.on("exit", () => {
-      res(stdout);
+      res(stdout.trim());
     });
   });
 }
@@ -26,12 +27,17 @@ fastify.post("/", async (req, _) => {
   const tsCode = String(req.body);
   console.log(tsCode);
 
-  const js = transpile(tsCode);
+  const js = transpile(tsCode, {
+    experimentalDecorators: true,
+    emitDecoratorMetadata: true,
+    module: ModuleKind.ES2015,
+    target: ScriptTarget.ES2015,
+  });
   console.log(js);
   const filename = randomUUID();
   await writeFile(filename, js);
   try {
-    const output = await cmd(`node ./${filename}`);
+    const output = await cmd(`zx --install --quiet ./${filename}`);
     console.log(output);
     return output;
   } finally {
@@ -41,7 +47,7 @@ fastify.post("/", async (req, _) => {
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ port: 3000, host: "0.0.0.0" });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
